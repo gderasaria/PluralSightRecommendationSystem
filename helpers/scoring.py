dc78e69b4f54611a9bb5880db796256f0965ddf9
unknown <gauravderasaria@outlook.com>
2018-08-22 02:28:29 -0400

    Created basic scoring suggestion on course views

--- helpers/scoring.py
+++ helpers/scoring.py
@@ -1,25 +1,66 @@
 import pandas as pd 
 import numpy as np 
+import pickle 
+from scipy.sparse.linalg import svds
+from sklearn.metrics.pairwise import pairwise_distances
+from operator import itemgetter
 
-
-class Scoring(Model):
+class Scoring:
 	''' 
 	Generates similarity score based on various features
 	'''
-	def __init__():
+	
+	def perform_svd(self,data_frame,k):
 		
-		pass 
+		U,S,Vt = svds(data_frame, k = 800)
+		U = pairwise_distances(U, metric='cosine')
+		U = pd.DataFrame(U, index = data_frame.index,\
+						  columns = data_frame.index)
+		return U 
 
-	def assessment_score():
+	def get_co_ocurrence(self,dataframe,H):
+
+		co_occurence_data = dataframe.dot(dataframe.T) # Compute co-occurence of courses amongst users
+		offset = 10**-10       	
+		co_occurence_data  =  co_occurence_data/H
+		co_occurence_data[co_occurence_data > 1]  = 1 
+		co_occurence_data[co_occurence_data  == 0] = offset #Adding offset to avoiding dividing by zero 
+
+		return co_occurence_data 
+
+	def assessment_score(data_frame):
+				
+		pass
+
+	def interests_score(user_interests):
 		
 		pass
 
-	def course_score():
+	def course_score(self,user_course_views):
 		
-		pass
+		user_course_views = pd.pivot_table(user_course_views, index= "user_handle",\
+                        				   columns = "course_id" ,\
+                        				   values="view_time_seconds", aggfunc = len)
+		user_course_views = user_course_views.replace(np.nan,0)
+		user_course_views[user_course_views != 0 ] = 1
 
-	def common_interests_score():
-		
-		pass 
+		#Run svd on the user_course matrix to get a user similarity matrix
+		user_similarity = self.perform_svd(user_course_views,800)
+		course_co_occurence = self.get_co_ocurrence(user_course_views,6)
+		user_similarity = user_similarity.div(course_co_occurence)	
+		course_scoring_dict = {}
+		for index,row in zip(user_similarity.index, user_similarity.iterrows()): 
+			course_scoring_dict[index] = []
+			for idx,i in zip(user_similarity.index,row[1]):
+				if index != idx :
+					course_scoring_dict[index].append((idx,i))
+			course_scoring_dict[index] = sorted(course_scoring_dict[index], key= itemgetter(1))
+			course_scoring_dict[index] = course_scoring_dict[index][:100]
+ 	
+ 		#conn  = sqlite3.connect("../data/processed/course_scoring_dict.sqlite")
+ 		#c = conn.cursor()
+		with open("../data/processed/course_scoring_dict.pickle",'wb') as file:
+			pickle.dump(course_scoring_dict,file, protocol = pickle.HIGHEST_PROTOCOL)
+ 
 
 
--- models/similarusers.py
+++ models/similarusers.py
@@ -1,24 +1,50 @@
 import pandas as pd
 import numpy as np 
+from operator import itemgetter
+import os 
+
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+from helpers.scoring import Scoring
+
+import sqlite3
+import pickle
+
+class SimilarUsers: 
+	
+	def __init__(self, user_id, train_mode):
+		self.user_id = user_id
+		self.train_mode = train_mode
+		self.scoring = Scoring()
 
 
+	def pre_process_data(self,file_path	):
+		"""Loads data from sqlite database to pre-process the data and store it in sqlite format"""
 
-Class Model():
+		conn = sqlite3.connect(file_path)
+		#course_tags = pd.read_sql("SELECT * FROM course_tags", conn)
+		user_assessment_scores = pd.read_sql("SELECT * FROM user_assessment_scores", conn)
+		user_course_views = pd.read_sql("SELECT * FROM user_course_views",conn)
+		user_interests = pd.read_sql("SELECT * FROM user_interests",conn)
+		conn.close()
 
-	def __init__():
+		self.scoring.course_score(user_course_views)
+		return
 
-		pass
+	def recommendation(self):
+		#print("inside")
+		with open('../data/processed/course_scoring_dict.pickle', 'rb') as handle:
+			#print("opened file")
+			course_scoring_dict = pickle.load(handle)
+			#print("completed loading") 
+		if self.user_id in course_scoring_dict:
+			#print("user_id exists")
+			print(course_scoring_dict[self.user_id][:5])
+			#print("done")
+		return 
 
-	def load_data():
-
-		pass
-
-	def pre_process_data():
-
-		pass
-
-	def recommendation():
-
-		pass
 
 
