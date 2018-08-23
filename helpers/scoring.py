7d1834e38f52811f05718b657bbc8f2dc31402c6
unknown <gauravderasaria@outlook.com>
2018-08-23 04:15:56 -0400

    Added assessment scoring function

--- helpers/scoring.py
+++ helpers/scoring.py
@@ -9,9 +9,9 @@ class Scoring:
 	''' 
 	Generates similarity score based on various features
 	'''
-	
+
 	def perform_svd(self,data_frame,k):
-		
+		"""Performs svd on the user dataframe and returns user similarity matrix """
 		U,S,Vt = svds(data_frame, k)
 		U = pairwise_distances(U, metric='cosine')
 		U = pd.DataFrame(U, index = data_frame.index,\
@@ -19,6 +19,7 @@ class Scoring:
 		return U 
 
 	def get_co_occurrence(self,dataframe,H):
+		"""" Helper to get the coocurrence of users from the dataframe  """
 
 		co_occurence_data = dataframe.dot(dataframe.T) # Compute co-occurence of courses amongst users
 		offset = 10**-10       	
@@ -30,6 +31,7 @@ class Scoring:
 
 	def generate_dict(self,dataframe):
 		"""Takes in a similarity dataframe and returns a dictionary of top 100 users for each user id"""
+
 		scoring_dict = {}
 		for index,row in zip(dataframe.index, dataframe.iterrows()): 
 			scoring_dict[index] = []
@@ -42,11 +44,42 @@ class Scoring:
 		return scoring_dict
 
 
-	def assessment_score(data_frame):
-				
-		pass
+	def assessment_score(self,user_assessment_data):
+		"""
+		Computes user similarity based on assessment scores 
+		"""
+		user_assessment = pd.pivot_table(user_assessment_data, index = "user_handle" , columns= "assessment_tag" ,\
+                                                  values="user_assessment_score")
+		user_assessment = user_assessment.apply(lambda x: x / np.sqrt(np.nansum(np.square(x))))
+		user_assessment = user_assessment.replace(np.nan,0)
+
+		offset = 10**-10
+		user_assessment = self.perform_svd(user_assessment, k =50)
+		
+		temp_table = pd.pivot_table(user_assessment_data, index = "user_handle", columns = "assessment_tag" ,\
+									  values = "user_assessment_date" ,  
+									  aggfunc = len )
+		H = 3 
+		co_occurence = temp_table.dot(temp_table.T)
+		co_occurence = co_occurence.replace(np.nan,0)
+		co_occurence = co_occurence.replace(0,offset)
+		co_occurence = co_occurence/H
+		co_occurence[co_occurence > 1] = 1
+
+		user_assessment = user_assessment.div(co_occurence)
+		
+		
+		interests_scoring_dict = self.generate_dict(user_assessment)
+		
+		#print(interests_scoring_dict[7487][:5])
+		with open("../data/processed/assessment_scoring_dict.pickle",'wb') as file:
+			pickle.dump(interests_scoring_dict, file, protocol = pickle.HIGHEST_PROTOCOL)
+
 
 	def interests_score(self,user_interests):
+		"""
+		Computes user similarity based on user_interests
+		"""
 		
 		user_interests = pd.pivot_table(user_interests,index = "user_handle",\
 												  columns="interest_tag",values = "date_followed",aggfunc=len)
@@ -58,13 +91,15 @@ class Scoring:
 		interests_co_occurence = self.get_co_occurrence(user_interests, 8)
 		user_similarity = user_similarity.div(interests_co_occurence)	
 		interests_scoring_dict = self.generate_dict(user_similarity)
-
+		
 		with open("../data/processed/interests_scoring_dict.pickle",'wb') as file:
 			pickle.dump(interests_scoring_dict,file, protocol = pickle.HIGHEST_PROTOCOL)
- 
-
 
 	def course_score(self,user_course_views):
+		
+		""" 
+		Computes user similarity based on course_interests
+		"""
 		
 		user_course_views = pd.pivot_table(user_course_views, index= "user_handle",\
                         				   columns = "course_id" ,\
@@ -77,12 +112,8 @@ class Scoring:
 		course_co_occurence = self.get_co_occurrence(user_course_views,6)
 		user_similarity = user_similarity.div(course_co_occurence)	
 		course_scoring_dict = self.generate_dict(user_similarity)
-
- 	
+		
  		#conn  = sqlite3.connect("../data/processed/course_scoring_dict.sqlite")
  		#c = conn.cursor()
 		with open("../data/processed/course_scoring_dict.pickle",'wb') as file:
 			pickle.dump(course_scoring_dict,file, protocol = pickle.HIGHEST_PROTOCOL)
- 
-
-
