03093bcc055311e7e7545f22dbac094ea8cb0651
unknown <gauravderasaria@outlook.com>
2018-08-23 22:24:59 -0400

    Removing the temporary test file and uploading the train file

--- models/train.py
+++ models/train.py
@@ -1,7 +1,9 @@
 #A Temporary test file 
 from similarusers import SimilarUsers
 
-
-sim = SimilarUsers(7,True) 
+print("inside test")
+sim = SimilarUsers(7487,True)
+print("object created") 
 sim.pre_process_data("../data/raw/rawdata.db")
+print("pre_processing done")
 sim.recommendation()
